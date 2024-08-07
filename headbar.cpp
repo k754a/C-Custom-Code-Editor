ffa7235550c7b6173ca9f383deb7a69bbd402948
k754a <kallenms27@g.eips.ca>
2024-08-07 16:34:55 -0600

    Can almost load file and fixed bug

--- headbar.cpp
+++ headbar.cpp
@@ -8,8 +8,7 @@
 #include <filesystem>
 #include "Terminal.h"
 
-//make it gloabl
-//file strct
+// Global variables and structures
 struct FileNode {
     std::wstring name;
     bool isDirectory;
@@ -18,7 +17,6 @@ struct FileNode {
 };
 
 std::string currentFilePath; // C file path
-
 int fps;
 std::string fpsString;
 std::vector<FileNode> fileTree;
@@ -27,31 +25,31 @@ std::string content;
 std::vector<char> bufferContent;
 
 std::string CURRENT = "CURRENT";
+std::atomic<int> cout(0);
 
+// Function declarations
 void PrintFileContent(const FileNode& node);
 void ListFilesRecursively(const std::wstring& directory, FileNode& node);
 void OpenFile();
 std::string CWstrTostr(const std::wstring& wstr);
 void DisplayFile(const FileNode& node);
+void Renderbar();
+void incrementCout();
+std::string ReadFileToString(const std::string& filePath);
+void Settingsrender(); // Assuming this is defined elsewhere
+void RenderTerminal(float windowWidth, float windowHeight, float terminalHeight); // Assuming this is defined elsewhere
 
-
-std::atomic<int> cout(0); 
+// Function implementations
 
 void incrementCout() {
     while (true) {
-
         if (cout < 2) {
             std::cout << "count" << std::endl;
             cout++;
-
         }
-
         std::this_thread::sleep_for(std::chrono::seconds(5));
-
-
     }
 }
-
 
 void ListFilesRecursively(const std::wstring& directory, FileNode& node) {
     std::wstring searchPath = directory + L"\\*";
@@ -144,27 +142,27 @@ void DisplayFile(const FileNode& node) {
 }
 
 void PrintFileContent(const FileNode& node) {
-    std::ifstream fileStream(CWstrTostr(node.path));
+    std::ifstream fileStream(CWstrTostr(node.path), std::ios::binary | std::ios::ate);
     if (fileStream.is_open()) {
-        std::stringstream buffer;
-        buffer << fileStream.rdbuf();
+        std::streamsize fileSize = fileStream.tellg();
+        fileStream.seekg(0, std::ios::beg);
 
-        content = buffer.str();
-        bufferContent.resize(content.size() + content.size() + 1);
-        std::copy(content.begin(), content.end(), bufferContent.begin());
-        bufferContent.back() = '\0';
+        bufferContent.resize(fileSize);
+        if (fileStream.read(bufferContent.data(), fileSize)) {
+            content.assign(bufferContent.begin(), bufferContent.end());
+            currentFilePath = CWstrTostr(node.path);
+        }
+        else {
+            std::cerr << "ERROR: Failed to read file " << CWstrTostr(node.path) << std::endl;
+        }
+
         fileStream.close();
-
-
-        currentFilePath = CWstrTostr(node.path);
     }
     else {
         std::cerr << "ERROR: file_not_found, error 101 " << CWstrTostr(node.path) << std::endl;
         std::cerr << "Cannot find the selected file, sorry! " << CWstrTostr(node.path) << std::endl;
     }
 }
-
-
 
 std::string ReadFileToString(const std::string& filePath) {
     std::ifstream file(filePath);
@@ -178,12 +176,15 @@ std::string ReadFileToString(const std::string& filePath) {
     return buffer.str();
 }
 
-
-
-
-
-
 static float terminalHeightPercent = 0.2f;
+
+std::string wchar_to_string(const wchar_t* wstr) {
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+    std::string str(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, &str[0], size_needed, NULL, NULL);
+    return str;
+}
+
 void Renderbar() {
     float windowHeight = ImGui::GetIO().DisplaySize.y;
     float windowWidth = ImGui::GetIO().DisplaySize.x;
@@ -201,13 +202,6 @@ void Renderbar() {
 
     RenderTerminal(windowWidth, windowHeight, terminalHeight);
 
-
-
-
-
-
-
-
     bufferContent.resize(content.size() + bufferContent.size());
     ImGui::SetNextWindowSize(ImVec2(editorWidth + 10, editorHeight));
     ImGui::SetNextWindowPos(ImVec2(200.1f, 20)); // Lock top position
@@ -215,35 +209,24 @@ void Renderbar() {
     std::string line;
     std::stringstream fileBuffer;
 
-
     std::string newContent = fileBuffer.str();
     std::vector<char> newContentVec(newContent.begin(), newContent.end());
 
-
     bufferContent.insert(bufferContent.end(), newContentVec.begin(), newContentVec.end());
-
 
     bufferContent.push_back('\0');
 
-
     std::string buf = bufferContent.data();
-    //std::cout << buf.length() << std::endl;
+
     bufferContent.resize(buf.length() * buf.length());
-    //std::cout << buf.length() << std::endl;
 
     if (!bufferContent.empty()) {
-
         // Render the text editor
         ImGui::InputTextMultiline("##CodeEditor", bufferContent.data(), bufferContent.size(), ImVec2(-1.0f, -1.0f), ImGuiInputTextFlags_AllowTabInput);
-
-        // Shrink the buffer to fit the actual content
-        //bufferContent.shrink_to_fit();
     }
     else {
         ImGui::Text("Please Open A file...");
     }
-
-
 
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("File")) {
@@ -252,9 +235,8 @@ void Renderbar() {
                 OpenFile();
             }
 
-           
-            if (ImGui::MenuItem("Run")) {  // Run button with ">" label
-                std::string saveFilePath = "output.txt"; // Define your file path here
+            if (ImGui::MenuItem("Run")) {
+                std::string saveFilePath = "output.txt";
                 std::ofstream outFile(saveFilePath);
                 if (outFile.is_open()) {
                     outFile.write(bufferContent.data(), std::strlen(bufferContent.data()));
@@ -266,17 +248,7 @@ void Renderbar() {
                 }
             }
 
-            if (ImGui::MenuItem("Save")) {
-                std::ofstream outFile(currentFilePath, std::ios::binary); // Open the file in binary mode
-                if (outFile.is_open()) {
-                    outFile.write(bufferContent.data(), std::strlen(bufferContent.data()));
-                    outFile.close();
-                    std::cout << "Buffer content saved to " << currentFilePath << std::endl;
-                }
-                else {
-                    std::cerr << "Error: Unable to open file " << currentFilePath << std::endl;
-                }
-            }
+           
 
             ImGui::Separator();
             if (ImGui::MenuItem("Exit")) {
@@ -284,8 +256,6 @@ void Renderbar() {
             }
             ImGui::EndMenu();
         }
-
-        
 
         if (ImGui::BeginMenu("Edit")) {
             if (ImGui::MenuItem("Settings")) {
@@ -316,10 +286,40 @@ void Renderbar() {
             ImGui::EndMenu();
         }
 
+
         if (ImGui::BeginMenu("Run")) {
             if (ImGui::MenuItem("Run Script")) {
-                settings = true;
+
+                wchar_t path[MAX_PATH];
+                if (GetModuleFileName(NULL, path, MAX_PATH)) {
+                    std::string path_str = wchar_to_string(path);
+                    std::cout << "Executable Path: " << path_str << std::endl;
+                }
+                else {
+                    std::cerr << "Error retrieving path" << std::endl;
+                }
+
+                std::string saveFilePath = "script.py";
+
+                // Save buffer content to .py file
+                std::ofstream outFile(saveFilePath);
+                if (outFile.is_open()) {
+                    outFile.write(bufferContent.data(), bufferContent.size());
+                    outFile.close();
+                    std::cout << "Buffer content saved to " << saveFilePath << std::endl;
+                }
+                else {
+                    std::cerr << "Error: Unable to open file " << saveFilePath << std::endl;
+                    ImGui::EndMenu();
+                    return;
+                }
+
+                // Execute the saved Python file
+                ExecuteCommand("python " + saveFilePath);
             }
+
+            
+            
 
             ImGui::Separator();
             ImGui::EndMenu();
@@ -352,13 +352,6 @@ void Renderbar() {
 
         ImGui::EndMainMenuBar();
     }
-
 }
 
-
-
-
-
 //this would be wayyyy more cleaner if i could just leave all the funct on the bottom, but nope :(
-
-
